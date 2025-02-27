95d69bb014fb0bff84dce72e5b441f0101a208e9
JackKish2006 <jackkish2@gmail.com>
2025-02-27 23:48:51 +0000

    Project 1 - coverted alpha to radians

--- Project#1.cpp
+++ Project#1.cpp
@@ -1,3 +1,7 @@
+//Name 
+//Date 
+//Program
+//Program info/ algorithm steps
 #include <iostream>
 #include <cmath>
 using namespace std;
@@ -29,18 +33,18 @@ int main()
 //user gives muzzle launch angle 
     cout << "Enter muzzle angle: ";
     cin >> alpha;
+    alpha = M_PI / 180 * alpha;
 
     //this is gravity
     double g = 9.81;
-    double alpha_rad = 45.0 * (3.14159 / 180.0);
     //Starting with max height using the equation (Muzzle Velocity (V) V^2*(sin^2(alpha)/2*g))
-    double h_max = (muzzle_velocity * muzzle_velocity * sin(alpha_rad) * sin(alpha_rad)) / (2 * g);
+    double h_max = (muzzle_velocity * muzzle_velocity * sin(alpha) * sin(alpha)) / (2 * g);
     //Flight time equation (2* Muzzle velocity*(sin(alpha)/g))
-    double flight_time = (2 * muzzle_velocity * sin(alpha_rad)) / g;
+    double flight_time = ( 2 * muzzle_velocity * sin(alpha)) / g;
     //distance traveled given by (V^2*sin(2*alpha)/g)
-    double x_distance = (((muzzle_velocity * muzzle_velocity) * sin(2 * alpha_rad)) / g);
+    double x_distance = (((muzzle_velocity * muzzle_velocity) * sin(2 * alpha)) / g);
     // the max range is also given by the same equation as the distance traveled but at an optimal launch angle of 45 degrees
-    double max_range = (((muzzle_velocity * muzzle_velocity) * sin(2 * 45.0 * alpha_rad )) / g);
+    double max_range = ((muzzle_velocity * muzzle_velocity) * sin(2 * (45 * M_PI / 180 * alpha)) / g);
     //if the enterd target range is within the max range
     bool within_range = target_distance <= max_range;
 
